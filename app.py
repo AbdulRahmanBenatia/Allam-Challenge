d1ccff7aba584d94d417f9a180f39285a03018e5
AbdulRahmanBenatia <95504237+AbdulRahmanBenatia@users.noreply.github.com>
2024-10-01 12:43:45 +0300

    Update app.py
    Drop Down Fixed
    TextInput Fixed (bck)

--- app.py
+++ app.py
@@ -7,12 +7,49 @@ from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
-
+BUHOOR = [ #These are only 14
+    "السريع",
+    "الكامل",
+    "المتقارب",
+    "المتدارك",
+    "المنسرح",
+    "المديد",
+    "المجتث",
+    "الرمل",
+    "البسيط",
+    "الخفيف",
+    "الطويل",
+    "الوافر",
+    "الهزج",
+    "الرجز",
+    
+    # Adding the rest 2 meters, Note: Not in Metrec Dataset
+    "المقتضب",
+    "الخبب",
+]
 # Handling Arabic Text
 def reshape_text(text):
     reshaped_text = arabic_reshaper.reshape(text)
     bidi_text = get_display(reshaped_text)
     return bidi_text
+
+class ArabicTextInput(TextInput):
+    def __init__(self, **kwargs):
+        super(ArabicTextInput, self).__init__(**kwargs)
+        self.BACK = False  # Variable to check if external action is triggered
+
+    
+    def keyboard_on_key_down(self, window, keycode, text, modifiers):
+        if keycode[1] == 'backspace':
+            print("Backspace pressed!")
+            # Add a letter (e.g., 'X') when backspace is pressed
+            self.BACK = True
+            
+            # You can also control cursor positioning if needed, e.g.,
+            # self.cursor = (self.cursor[0] + 1, self.cursor[1])
+            # return "BACK"  # Return True to override default backspace behavior
+        # Call the parent method to retain other key functionalities
+        return super(ArabicTextInput, self).keyboard_on_key_down(window, keycode, text, modifiers)
 
 class MainPage(Screen):
     def __init__(self, **kwargs):
@@ -67,7 +104,7 @@ class Page1(Screen):
         layout.add_widget(label)
 
         # Create a TextInput for Arabic text
-        self.text_input = TextInput(hint_text=reshape_text('أدخل البيت هنا'), font_size='24sp', size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
+        self.text_input = ArabicTextInput(hint_text=reshape_text('أدخل البيت هنا'), font_size='24sp', size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
         layout.add_widget(self.text_input)
 
         # Create a Label to display reshaped text
@@ -75,23 +112,48 @@ class Page1(Screen):
         layout.add_widget(self.output_label)
 
         # Bind the on_text event to update the output label and input text
-        self.text_input.bind(on_text=self.update_output)
+        # self.text_input.bind(on_text=self.update_output)
+        self.text_input.bind(text=self.update_output)
 
+        # Submit button
+        submit_button = Button(text=reshape_text("تأكيد"), size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
+        # submit_button.bind(on_press=self.on_submit)
+        layout.add_widget(submit_button)
+        
         # Back button
         back_button = Button(text=reshape_text('رجوع'), size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
         back_button.bind(on_press=lambda x: setattr(self.manager, 'current', 'main'))
         layout.add_widget(back_button)
 
         self.add_widget(layout)
-
-    def update_output(self, instance, value):
-        # Update the output label to show the reshaped text
-        reshaped_text = reshape_text(value)
-        self.output_label.text = reshaped_text
-        # Reshape text in the input field
-        self.text_input.text = reshaped_text  # Update the input field with reshaped text
         
-        print(f"Current input: {value}")  # Print the unreshaped input
+        self.input_list = []
+        self.last_value = ''
+        self.full_verse = '' 
+
+
+    def update_output(self, instance, value):        
+        # Reshape Arabic text from the raw input
+        # reshaped_text = arabic_reshaper.reshape(self.raw_text)  # Reshape the Arabic text for proper display
+        print("Val : ",value)
+        print("Reshaped: ",reshape_text(value))
+        
+        if self.text_input.BACK:
+            self.input_list = self.input_list[:-1]
+            value=value[:-1]
+            
+        else:
+            self.input_list.append(value[-1])
+            # Unbind to prevent recursion error when updating text
+            instance.unbind(text=self.update_output)
+            # instance.text = bidi_text
+            till_now = reshape_text(''.join(self.input_list))
+            if(self.input_list[-1]!=' '):
+                instance.text = till_now
+        # Rebind after updating the text
+        
+        instance.bind(text=self.update_output)
+        self.full_verse = value
 
 
 class Page2(Screen):
@@ -102,18 +164,14 @@ class Page2(Screen):
         dropdown = DropDown()
 
         # Arabic options for the dropdown
-        options = [
-            'خيار ١', 'خيار ٢', 'خيار ٣', 'خيار ٤',
-            'خيار ٥', 'خيار ٦', 'خيار ٧', 'خيار ٨',
-            'خيار ٩', 'خيار ١٠', 'خيار ١١', 'خيار ١٢',
-            'خيار ١٣', 'خيار ١٤', 'خيار ١٥', 'خيار ١٦'
-        ]
+        options = BUHOOR
 
         # Add options to dropdown
         for option in options:
-            btn = Button(text=reshape_text(option), size_hint_y=None, height=44)
+            btn = Button(text=reshape_text(option), size_hint_y=None, height=44,font_name="Amiri-Regular.ttf")
             btn.bind(on_release=lambda btn: self.select_option(btn.text, dropdown))
             dropdown.add_widget(btn)
+
 
         main_button = Button(text=reshape_text('اختر بحرا'), size_hint=(1, 0.1), font_name="Amiri-Regular.ttf")
         main_button.bind(on_release=dropdown.open)
@@ -136,17 +194,61 @@ class Page3(Screen):
         super(Page3, self).__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
+        # Title label
         label = Label(text=reshape_text("أدخل البيت المراد شرحه"), font_size='24sp', halign='center', valign='middle', font_name="Amiri-Regular.ttf")
         layout.add_widget(label)
 
-        text_input = TextInput(hint_text=reshape_text('أدخل البيت هنا'), font_size='24sp', size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
-        layout.add_widget(text_input)
+        # Create a TextInput for Arabic text
+        self.text_input = ArabicTextInput(hint_text=reshape_text('أدخل البيت هنا'), font_size='24sp', size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
+        layout.add_widget(self.text_input)
 
+        # Create a Label to display reshaped text
+        self.output_label = Label(text='', font_size='24sp', size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
+        layout.add_widget(self.output_label)
+
+        # Bind the on_text event to update the output label and input text
+        # self.text_input.bind(on_text=self.update_output)
+        self.text_input.bind(text=self.update_output)
+
+        # Submit button
+        submit_button = Button(text=reshape_text("تأكيد"), size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
+        # submit_button.bind(on_press=self.on_submit)
+        layout.add_widget(submit_button)
+        
+        # Back button
         back_button = Button(text=reshape_text('رجوع'), size_hint=(1, 0.2), font_name="Amiri-Regular.ttf")
         back_button.bind(on_press=lambda x: setattr(self.manager, 'current', 'main'))
         layout.add_widget(back_button)
 
         self.add_widget(layout)
+        
+        self.input_list = []
+        self.last_value = ''
+        self.full_verse = '' 
+
+
+    def update_output(self, instance, value):        
+        # Reshape Arabic text from the raw input
+        # reshaped_text = arabic_reshaper.reshape(self.raw_text)  # Reshape the Arabic text for proper display
+        print("Val : ",value)
+        print("Reshaped: ",reshape_text(value))
+        
+        if self.text_input.BACK:
+            self.input_list = self.input_list[:-1]
+            value=value[:-1]
+            
+        else:
+            self.input_list.append(value[-1])
+            # Unbind to prevent recursion error when updating text
+            instance.unbind(text=self.update_output)
+            # instance.text = bidi_text
+            till_now = reshape_text(''.join(self.input_list))
+            if(self.input_list[-1]!=' '):
+                instance.text = till_now
+        # Rebind after updating the text
+        
+        instance.bind(text=self.update_output)
+        self.full_verse = value
 
 class Page4(Screen):
     def __init__(self, **kwargs):
@@ -156,16 +258,10 @@ class Page4(Screen):
         dropdown = DropDown()
 
         # Arabic options for the dropdown
-        options = [
-            'خيار ١', 'خيار ٢', 'خيار ٣', 'خيار ٤',
-            'خيار ٥', 'خيار ٦', 'خيار ٧', 'خيار ٨',
-            'خيار ٩', 'خيار ١٠', 'خيار ١١', 'خيار ١٢',
-            'خيار ١٣', 'خيار ١٤', 'خيار ١٥', 'خيار ١٦'
-        ]
-
+        options = BUHOOR
         # Add options to dropdown
         for option in options:
-            btn = Button(text=reshape_text(option), size_hint_y=None, height=44)
+            btn = Button(text=reshape_text(option), size_hint_y=None, height=44,font_name="Amiri-Regular.ttf")
             btn.bind(on_release=lambda btn: self.select_option(btn.text, dropdown))
             dropdown.add_widget(btn)
 
